54007029167cab4a2622b7006462d3ba320efb40
Darshan <darshanrajpurohit47@gmail.com>
2023-01-11 02:55:08 +0530

    new contract added

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6D8646d51A87019816b1588d594CD0cE469a45be";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-tcse.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
