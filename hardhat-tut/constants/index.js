9f884315d222ca0339d7eb69236b0149ac3559cb
Terra-Credit <terracredit448@gmail.com>
2023-01-19 12:51:48 +0200

    Update index.js

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xf175F93EC2e6Be07Cb220C0BE02bB4dDE8F959CD";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-tcse.vercel.app/api/";
+const METADATA_URL = "https://bejewelled-mochi-28dd6e.netlify.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
